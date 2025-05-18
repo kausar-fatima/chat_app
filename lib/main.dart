ba0ef9f575bb524d7c849cdb4b2d2a6836729402
kausar-fatima <fatimakosar5@gmail.com>
2025-05-18 12:12:01 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
-import 'package:chat_app/pages/HomePage.dart';
-import 'package:chat_app/pages/widgets/ChatPage.dart';
+import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
@@ -12,17 +20,27 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 243, 231, 197),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromARGB(255, 243, 231, 197),
-            foregroundColor: Colors.black,
-          ),
+      debugShowCheckedModeBanner: false,
+      title: 'Let us Chat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 63, 17, 177),
         ),
-        routes: {
-          "/": (context) => const HomePage(),
-          "chatPage": (context) => const ChatPage(),
-        });
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          } else {
+            return const AuthScreen();
+          }
+        },
+      ),
+    );
   }
 }
